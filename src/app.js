a91f68b9c65f6a0c1c7af92f770be0017d163127
MeSailor <themicroline@gmail.com>
2024-02-20 16:05:36 +0400

    fixed bug with require path in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3002;
-const dataGenerator = require("./service/dataGenerator");
+const dataGenerator = require("./services/dataGenerator");
 
 app.use(express.json());
 
